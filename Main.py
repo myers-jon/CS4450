f55f418d6659babd044b750f8ca1e19644e99219
myers-jon <jtm6hb@umsystem.edu>
2022-11-09 19:24:53 -0600

    crudely working version of main and new_grammar

--- Main.py
+++ Main.py
@@ -2,14 +2,27 @@ import sys
 from antlr4 import *
 from new_grammarLexer import new_grammarLexer
 from new_grammarParser import new_grammarParser
+from new_grammarListener import new_grammarListener
+
+
+
+class new_grammarPrintListener(new_grammarListener):
+    def enterParse(self, ctx):
+        print("> %s" % ctx.block())
+
+
 
 def main(argv):
 
-    lexer = new_grammarLexer("var = 3")
+
+    lexer = new_grammarLexer(StdinStream())
     tokens = CommonTokenStream(lexer)
     parser = new_grammarParser(tokens)
-    tree = parser.prog()
-    print(tree.toStringTree(recog=parser))
+    tree = parser.parse()
+    printer = new_grammarPrintListener()
+    walker = ParseTreeWalker()
+    walker.walk(printer, tree)
+
 
 if __name__ == '__main__':
     main(sys.argv)
