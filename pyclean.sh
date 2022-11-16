81d7201a6dbd539d29835b73e789dde50875c789
myers-jon <jtm6hb@umsystem.edu>
2022-11-16 15:46:43 -0600

    added a clean bash function

---
+++ pyclean.sh
@@ -0,0 +1,8 @@
+rm ./new_grammar.interp
+rm ./new_grammarLexer.interp
+rm ./new_grammarLexer.py
+rm ./new_grammarLexer.tokens
+rm ./new_grammarListener.py
+rm ./new_grammarParser.py
+rm ./new_grammar.tokens
+
